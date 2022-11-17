46e04a8ab5b0d23d37f47a223f12f21a8e0aedfe
Chris <boyd111c@gmail.com>
2022-11-17 10:29:25 -0600

    switch to rollup. use iwo-renderer from npm upload assets

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import {glMatrix, mat4, vec3} from 'gl-matrix';
-import * as IWO from 'ts-pbr-renderer/src/iwo';
-// @ts-ignore
-import Stats from 'stats.js';
+import {glMatrix, mat4, vec3} from "gl-matrix";
+import * as IWO from "iwo-renderer";
 
 let canvas: HTMLCanvasElement;
 let gl: WebGL2RenderingContext;
@@ -12,86 +10,13 @@ const proj_matrix: mat4 = mat4.create();
 const cPos: vec3 = vec3.fromValues(0.5, 8, 9.0);
 let camera: IWO.Camera;
 
-let mouse_x_total = 0;
-let mouse_y_total = 0;
-const keys: Array<boolean> = [];
-
 let grid: IWO.MeshInstance;
 let renderer: IWO.Renderer;
+let fps_control: IWO.FPSControl;
 
-document.getElementById('loading-text-wrapper')!.remove();
-
-const moveCallback = (e: MouseEvent): void => {
-    const movementX = e.movementX || 0;
-    const movementY = e.movementY || 0;
-    if (e.buttons == 1) {
-        mouse_x_total += movementX;
-        mouse_y_total += movementY;
-    }
-};
-
-const initGL = (): WebGL2RenderingContext => {
-    try {
-        gl = canvas.getContext('webgl2') as WebGL2RenderingContext;
-    } catch (e) {
-        throw new Error('GL init error:\n' + e);
-    }
-
-    if (!gl) {
-        alert('WebGL is not available on your browser.');
-    }
-    return gl;
-};
-
-const initScene = (): void => {
-    const plane_geom = new IWO.PlaneGeometry(100, 100, 1, 1, true);
-    const plane_mesh = new IWO.Mesh(gl, plane_geom);
-
-    //GRID
-    const grid_mat = new IWO.GridMaterial(50);
-    grid = new IWO.MeshInstance(plane_mesh, grid_mat);
-};
-
-const drawScene = (): void => {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    camera.getViewMatrix(view_matrix);
-    renderer.setPerFrameUniforms(view_matrix, proj_matrix);
-
-    gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    grid.render(renderer, view_matrix, proj_matrix);
-    gl.disable(gl.BLEND);
-};
-
-const update = (): void => {
-    if (keys[87]) camera.processKeyboard(IWO.Camera_Movement.FORWARD, 0.001);
-    else if (keys[83]) camera.processKeyboard(IWO.Camera_Movement.BACKWARD, 0.001);
-    if (keys[65]) camera.processKeyboard(IWO.Camera_Movement.LEFT, 0.001);
-    else if (keys[68]) camera.processKeyboard(IWO.Camera_Movement.RIGHT, 0.001);
-    if (keys[82]) camera.lookAt(vec3.fromValues(0, 0, 0));
-    if (keys[32]) camera.processKeyboard(IWO.Camera_Movement.UP, 0.001);
-
-    camera.processMouseMovement(-mouse_x_total, -mouse_y_total, true);
-    mouse_x_total = 0;
-    mouse_y_total = 0;
-
-    drawScene();
-    requestAnimationFrame(update);
-};
-
-(function loadWebGL(): void {
-    const stats = new Stats();
-    document.body.appendChild(stats.dom);
-    requestAnimationFrame(function loop() {
-        stats.update();
-        requestAnimationFrame(loop);
-    });
-
-    canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    document.addEventListener('mousemove', moveCallback, false);
-
-    gl = initGL();
+await (async function () {
+    canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    gl = IWO.initGL(canvas);
 
     renderer = new IWO.Renderer(gl);
 
@@ -108,10 +33,11 @@ const update = (): void => {
         );
     }
 
-    window.addEventListener('resize', resizeCanvas, false);
+    window.addEventListener("resize", resizeCanvas, false);
     resizeCanvas();
 
     camera = new IWO.Camera(cPos);
+    fps_control = new IWO.FPSControl(camera);
 
     gl.clearColor(173 / 255, 196 / 255, 221 / 255, 1.0);
     gl.enable(gl.DEPTH_TEST);
@@ -125,21 +51,41 @@ const update = (): void => {
     const sun_intensity = 9;
     const sun_color = [(sun_intensity * 254) / 255, (sun_intensity * 238) / 255, (sun_intensity * 224) / 255];
 
-    const pbrShader = IWO.PBRMaterial.Shader;
+    const pbrShader = renderer.getorCreateShader(IWO.ShaderSource.PBR);
     pbrShader.use();
-    pbrShader.setUniform('u_lights[0].position', [sun_dir[0], sun_dir[1], sun_dir[2], 0]);
-    pbrShader.setUniform('u_lights[0].color', sun_color);
-    pbrShader.setUniform('u_light_count', 1);
+    pbrShader.setUniform("u_lights[0].position", [sun_dir[0], sun_dir[1], sun_dir[2], 0]);
+    pbrShader.setUniform("u_lights[0].color", sun_color);
+    pbrShader.setUniform("u_light_count", 1);
 
     initScene();
 
     requestAnimationFrame(update);
 })();
 
-window.onkeydown = function(e: KeyboardEvent): void {
-    keys[e.keyCode] = true;
-};
+function initScene() {
+    const plane_geom = new IWO.PlaneGeometry(100, 100, 1, 1, true);
+    const plane_mesh = new IWO.Mesh(gl, plane_geom);
 
-window.onkeyup = function(e: KeyboardEvent): void {
-    keys[e.keyCode] = false;
-};
+    //GRID
+    const grid_mat = new IWO.GridMaterial();
+    grid = new IWO.MeshInstance(plane_mesh, grid_mat);
+}
+
+function drawScene() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    camera.getViewMatrix(view_matrix);
+    renderer.setPerFrameUniforms(view_matrix, proj_matrix);
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+    grid.render(renderer, view_matrix, proj_matrix);
+    gl.disable(gl.BLEND);
+}
+
+function update() {
+    fps_control.update();
+    drawScene();
+    requestAnimationFrame(update);
+}
