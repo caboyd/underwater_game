eb7d2517ec2e908c991d764bbe84ccef36987e78
Chris <boyd111c@gmail.com>
2022-11-22 12:39:48 -0600

    optimization: generate meshes on the fly fix noise formula

--- src/heightmap/HeightMap.ts
+++ src/heightmap/HeightMap.ts
@@ -20,7 +20,7 @@ const DefaultHeightMapOptions: HeightMapOptions = {
 
 type ActiveHeightMapMesh = {
     active: boolean;
-    mesh: MeshInstance;
+    mesh?: MeshInstance;
 };
 
 type Result_SBFW = {
@@ -37,6 +37,9 @@ const temp_fov_dir1 = vec2.create();
 const temp_fov_dir2 = vec2.create();
 const temp_pos_to_chunk_center = vec2.create();
 const temp_chunk_center = vec2.create();
+const temp_xy_vec = vec2.create();
+const temp_half_width_vec = vec2.create();
+const result_sbfw = {s: 0, b: 0, f: 0, w: 0};
 
 export class HeightMap implements HeightMapOptions {
     public readonly floor_meshes: ActiveHeightMapMesh[][];
@@ -47,6 +50,12 @@ export class HeightMap implements HeightMapOptions {
     readonly chunk_width_z: number;
     public material: PBRMaterial;
 
+    private readonly perlin: Perlin;
+    private readonly worley: Worley;
+    private readonly half_width: number;
+    private ceiling_index_buffer: IndexBuffer | undefined;
+    private floor_index_buffer: IndexBuffer | undefined;
+
     constructor(gl: WebGL2RenderingContext, options?: Partial<HeightMapOptions>) {
         const opt = {...DefaultHeightMapOptions, ...options};
 
@@ -54,40 +63,52 @@ export class HeightMap implements HeightMapOptions {
         this.z_chunks = opt.z_chunks;
         this.chunk_width_x = opt.chunk_width_x;
         this.chunk_width_z = opt.chunk_width_z;
-        this.floor_meshes = [];
-        this.ceiling_meshes = [];
+        this.floor_meshes = new Array(this.z_chunks)
+            .fill(new Array())
+            .map(() => new Array(this.x_chunks).fill(new Array()).map(() => ({active: false} as ActiveHeightMapMesh)));
+        this.ceiling_meshes = new Array(this.z_chunks)
+            .fill(new Array())
+            .map(() => new Array(this.x_chunks).fill(new Array()).map(() => ({active: false} as ActiveHeightMapMesh)));
         this.material = new PBRMaterial([1, 1, 1], 0.0, 0);
+        this.perlin = new Perlin(40, 70);
+        this.worley = new Worley(30, 0.5);
 
-        this.init(gl);
+        const width = this.chunk_width_x * this.x_chunks;
+        this.half_width = width / 2;
+        vec2.set(temp_half_width_vec, this.half_width, this.half_width);
+
+        //this.init(gl);
+    }
+
+    private generateChunkAt(gl: WebGL2RenderingContext, x: number, z: number) {
+        const ceiling_chunk = new HeightMapChunk(x * this.chunk_width_x, z * this.chunk_width_z, this.ceiling_noise(), {
+            x_width: this.chunk_width_x,
+            z_width: this.chunk_width_z,
+            tex_x_cells: 2,
+            tex_z_cells: 2,
+            flip_y: true,
+        });
+        const floor_chunk = new HeightMapChunk(x * this.chunk_width_x, z * this.chunk_width_z, this.floor_noise(), {
+            x_width: this.chunk_width_x,
+            z_width: this.chunk_width_z,
+            tex_x_cells: 2,
+            tex_z_cells: 2,
+        });
+
+        let m = new Mesh(gl, ceiling_chunk, {reuse_index_buffer: this.ceiling_index_buffer});
+        if (!this.ceiling_index_buffer) this.ceiling_index_buffer = m.index_buffer;
+        this.ceiling_meshes[z][x].mesh = new MeshInstance(m, this.material);
+        m = new Mesh(gl, floor_chunk, {reuse_index_buffer: this.floor_index_buffer});
+        if (!this.floor_index_buffer) this.floor_index_buffer = m.index_buffer;
+        this.floor_meshes[z][x].mesh = new MeshInstance(m, this.material);
     }
 
     private init(gl: WebGL2RenderingContext) {
-        const perlin = new Perlin(40, 70);
-        // const perlin2 = new Perlin(12, 12);
-        const worley = new Worley(30, 0.5);
-        const width = this.chunk_width_x * this.x_chunks;
-        const half_width = width / 2;
-        const half_width_vec = vec2.fromValues(half_width, half_width);
-        const xy_vec = vec2.create();
-        const result_sbfw = {s: 0, b: 0, f: 0, w: 0};
-
-        const ceiling_func = (x: number, y: number) => {
-            calculateHeightVars(result_sbfw, x, y);
-            const {s, b, f, w} = result_sbfw;
-
-            if (s <= 0) return b - 3;
-            return b * f + s - (w * w * f) / s;
-        };
-        const floor_func = (x: number, y: number) => {
-            calculateHeightVars(result_sbfw, x, y);
-            const {s, b, f, w} = result_sbfw;
-
-            if (s <= 0) return b + 3;
-            return b * f - s + (w * w * f) / s;
-        };
-
         let ceiling_index_buffer: IndexBuffer | undefined;
         let floor_index_buffer: IndexBuffer | undefined;
+
+        const floor_func = this.floor_noise();
+        const ceiling_func = this.ceiling_noise();
 
         for (let z = 0; z < this.z_chunks; z++) {
             this.floor_meshes[z] = [];
@@ -96,55 +117,73 @@ export class HeightMap implements HeightMapOptions {
                 const ceiling_chunk = new HeightMapChunk(x * this.chunk_width_x, z * this.chunk_width_z, ceiling_func, {
                     x_width: this.chunk_width_x,
                     z_width: this.chunk_width_z,
-                    tex_x_cells: 2,
-                    tex_z_cells: 2,
+                    tex_x_cells: 5,
+                    tex_z_cells: 5,
                     flip_y: true,
                 });
                 const floor_chunk = new HeightMapChunk(x * this.chunk_width_x, z * this.chunk_width_z, floor_func, {
                     x_width: this.chunk_width_x,
                     z_width: this.chunk_width_z,
-                    tex_x_cells: 2,
-                    tex_z_cells: 2,
+                    tex_x_cells: 5,
+                    tex_z_cells: 5,
                 });
-                //this.ceiling_chunks[z].push(ceiling_chunk);
-                //this.floor_chunks[z].push(floor_chunk);
 
                 let m = new Mesh(gl, ceiling_chunk, {reuse_index_buffer: ceiling_index_buffer});
                 if (!ceiling_index_buffer) ceiling_index_buffer = m.index_buffer;
-                this.ceiling_meshes[z].push({mesh: new MeshInstance(m, this.material), active: false});
+                this.ceiling_meshes[z][x].mesh = new MeshInstance(m, this.material);
                 m = new Mesh(gl, floor_chunk, {reuse_index_buffer: floor_index_buffer});
                 if (!floor_index_buffer) floor_index_buffer = m.index_buffer;
-                this.floor_meshes[z].push({mesh: new MeshInstance(m, this.material), active: false});
+                this.floor_meshes[z][x].mesh = new MeshInstance(m, this.material);
             }
-        }
-
-        function calculateHeightVars(result: Result_SBFW, x: number, y: number) {
-            //[-35,35]
-            const b = perlin.noise(x, y) / 35;
-            const w = worley.noise(x, y);
-            let s = 0;
-            let f = 0;
-            //distance to center
-            vec2.set(xy_vec, x, y);
-            const d = vec2.dist(xy_vec, half_width_vec);
-
-            if (d > half_width) s = 0; // no tunnls in corners of map
-            else if (d > half_width * 0.8)
-                s = Math.pow(((half_width - d) / half_width) * 0.2, 0.6) * 3.0; // tunnels near map edge
-            else if (d > half_width * 0.4) s = 3.0; // tunnels are constant size
-            else s = 3.0 + (1.0 + Math.cos((d / half_width) * 0.4 * Math.PI)) * 10; // cavern in middle
-
-            if (d > half_width * 0.4) f = 1.0; // not in cavern
-            else f = 0.75 - Math.cos((d / half_width) * 0.4 * Math.PI) * 0.25; // in cavern
-            result.s = s;
-            result.b = b;
-            result.f = f;
-            result.w = w;
-            return result;
         }
     }
 
+    private calculateHeightVars(result: Result_SBFW, x: number, y: number) {
+        //[-35,35]
+        const b = this.perlin.noise(x, y) - 35;
+        const w = this.worley.noise(x, y);
+        const half_w = this.half_width;
+        let s = 0;
+        let f = 0;
+        //distance to center
+        vec2.set(temp_xy_vec, x, y);
+        const d = vec2.dist(temp_xy_vec, temp_half_width_vec);
+
+        if (d > half_w) s = 0; // no tunnls in corners of map
+        else if (d > half_w * 0.8) s = Math.pow((half_w - d) / (half_w * 0.2), 0.6) * 3.0; // tunnels near map edge
+        else if (d > half_w * 0.4) s = 3.0; // tunnels are constant size
+        else s = 3.0 + (1.0 + Math.cos((d / (half_w * 0.4)) * Math.PI)) * 10; // cavern in middle
+
+        if (d > half_w * 0.4) f = 1.0; // not in cavern
+        else f = 0.75 - Math.cos((d / (half_w * 0.4)) * Math.PI) * 0.25; // in cavern
+        result.s = s;
+        result.b = b;
+        result.f = f;
+        result.w = w;
+        return result;
+    }
+
+    private floor_noise() {
+        return (x: number, y: number) => {
+            this.calculateHeightVars(result_sbfw, x, y);
+            const {s, b, f, w} = result_sbfw;
+
+            if (s <= 0) return b + 3;
+            return b * f - s + (w * w * f) / s;
+        };
+    }
+    private ceiling_noise() {
+        return (x: number, y: number) => {
+            this.calculateHeightVars(result_sbfw, x, y);
+            const {s, b, f, w} = result_sbfw;
+
+            if (s <= 0) return b - 3;
+            return b * f + s - (w * w * f) / s;
+        };
+    }
+
     public activateMeshesInView(
+        gl: WebGL2RenderingContext,
         pos: vec3,
         dir: vec3,
         fov_radians: number,
@@ -187,7 +226,7 @@ export class HeightMap implements HeightMapOptions {
                 const pos_angle_1 = vec2.angle(temp_pos_to_chunk_center, temp_fov_dir1);
                 const pos_angle_2 = vec2.angle(temp_pos_to_chunk_center, temp_fov_dir2);
                 if (pos_angle_1 < angle && pos_angle_2 < angle) {
-                    this.activateMesh(x, z);
+                    this.activateMesh(gl, x, z);
                 }
             }
         }
@@ -197,131 +236,28 @@ export class HeightMap implements HeightMapOptions {
         let z_index = Math.floor(pos[2] / this.chunk_width_z);
         for (let z = z_index - cell_radius; z <= z_index + cell_radius; z++) {
             for (let x = x_index - cell_radius; x <= x_index + cell_radius; x++) {
-                if (this.ceiling_meshes[z] && this.ceiling_meshes[z][x]) this.ceiling_meshes[z][x].active = true;
-                if (this.floor_meshes[z] && this.floor_meshes[z][x]) this.floor_meshes[z][x].active = true;
+                if (
+                    this.ceiling_meshes[z] &&
+                    this.ceiling_meshes[z][x] &&
+                    this.floor_meshes[z] &&
+                    this.floor_meshes[z][x]
+                )
+                    this.activateMesh(gl, x, z);
             }
         }
     }
 
-    private activateMesh(x: number, z: number) {
-        this.ceiling_meshes[z][x].active = true;
-        this.floor_meshes[z][x].active = true;
+    private activateMesh(gl: WebGL2RenderingContext, x: number, z: number) {
+        const c = this.ceiling_meshes[z][x];
+        c.active = true;
+        if (!c.mesh) this.generateChunkAt(gl, x, z);
+        const f = this.floor_meshes[z][x];
+        f.active = true;
+        if (!f.mesh) this.generateChunkAt(gl, x, z);
     }
 
     private deactiveMesh(x: number, z: number) {
         this.ceiling_meshes[z][x].active = false;
         this.floor_meshes[z][x].active = false;
-    }
-
-    private deactivateAllHeightMapMeshes() {
-        for (const arr of this.ceiling_meshes) {
-            for (const mesh of arr) mesh.active = false;
-        }
-        for (const arr of this.floor_meshes) {
-            for (const mesh of arr) mesh.active = false;
-        }
-    }
-
-    // public getMeshesInView(pos: vec3, dir: vec3, fov: number, cell_range: number): MeshInstance[] {
-    //     const result_meshes: MeshInstance[] = [];
-
-    //     const dir2 = vec2.fromValues(dir[0], dir[2]);
-    //     vec2.normalize(dir2, dir2);
-    //     const fov_dir_1 = vec2.rotate(vec2.create(), dir2, [0, 0], -glMatrix.toRadian(fov / 2));
-    //     const fov_dir_2 = vec2.rotate(vec2.create(), dir2, [0, 0], glMatrix.toRadian(fov / 2));
-
-    //     const cells_in_range_fov_dir_1: vec2[] = [];
-    //     const cells_in_range_fov_dir_2: vec2[] = [];
-
-    //     const MAX_DIST_SQ = this.chunk_width_x * cell_range + this.chunk_width_z * cell_range;
-    //     const start_pos = vec2.fromValues(pos[0], pos[2]);
-    //     let march_pos = vec2.fromValues(pos[0], pos[2]);
-
-    //     //march to find up to cell_range cells in fov_dir_1 direction
-    //     for (let i = 0; i < cell_range; i++) {
-    //         while (vec2.sqrDist(start_pos, march_pos) < MAX_DIST_SQ) {
-    //             vec2.add(march_pos, march_pos, fov_dir_1);
-    //             const ix = Math.floor(march_pos[0] / this.chunk_width_x);
-    //             const iz = Math.floor(march_pos[1] / this.chunk_width_z);
-    //             const exists = cells_in_range_fov_dir_1.find((v) => v[0] === ix && v[1] === iz);
-    //             //add a new found cell
-    //             if (!exists) {
-    //                 cells_in_range_fov_dir_1.push([ix, iz]);
-    //                 break;
-    //             }
-    //         }
-    //     }
-
-    //     march_pos = vec2.fromValues(pos[0], pos[2]);
-    //     //march to find up to cell_range cells in fov_dir_2 direction
-    //     for (let i = 0; i < cell_range; i++) {
-    //         while (vec2.sqrDist(start_pos, march_pos) < MAX_DIST_SQ) {
-    //             vec2.add(march_pos, march_pos, fov_dir_2);
-    //             const ix = Math.floor(march_pos[0] / this.chunk_width_x);
-    //             const iz = Math.floor(march_pos[1] / this.chunk_width_z);
-    //             const exists = cells_in_range_fov_dir_2.find((v) => v[0] === ix && v[1] === iz);
-    //             //add a new found cell
-    //             if (!exists) {
-    //                 cells_in_range_fov_dir_2.push([ix, iz]);
-    //                 break;
-    //             }
-    //         }
-    //     }
-
-    //     const cells: vec2[] = [];
-    //     cells.push(vec2.fromValues(Math.floor(pos[0] / this.chunk_width_x), Math.floor(pos[2] / this.chunk_width_z)));
-
-    //     //find all off the cells between the two lines
-    //     for (let i = 0; i < cell_range; i++) {
-    //         const cell_1 = cells_in_range_fov_dir_1[i];
-    //         const cell_2 = cells_in_range_fov_dir_2[i];
-
-    //         do {
-
-    //         } while (cell_1[0] !== cell_2[0] || cell_1[1] !== cell_2[1]);
-    //         continue;
-    //     }
-
-    //     //collect meshes for cell in the cone
-
-    //     return result_meshes;
-    // }
-
-    public getMeshesInRange(pos: vec3, dir: vec3, cell_radius: number): MeshInstance[] {
-        //get x_index, z_index of cell currently in
-        let x_index = Math.floor(pos[0] / this.chunk_width_x);
-        let z_index = Math.floor(pos[2] / this.chunk_width_z);
-
-        const dir2 = vec2.fromValues(dir[0], dir[2]);
-        vec2.normalize(dir2, dir2);
-
-        const offset = cell_radius > 1 ? cell_radius - 1 : 0;
-
-        //move center based on forward direction
-        if (vec2.dot(dir2, [1, 0]) > 0.5) {
-            x_index += offset;
-        } else if (vec2.dot(dir2, [-1, 0]) > 0.5) {
-            x_index -= offset;
-        }
-
-        if (vec2.dot(dir2, [0, 1]) > 0.5) {
-            z_index += offset;
-        } else if (vec2.dot(dir2, [0, -1]) > 0.5) {
-            z_index -= offset;
-        }
-
-        const result_meshes = [];
-
-        for (let z = z_index - cell_radius; z <= z_index + cell_radius; z++) {
-            for (let x = x_index - cell_radius; x <= x_index + cell_radius; x++) {
-                if (this.ceiling_meshes[z] && this.ceiling_meshes[z][x]) {
-                    result_meshes.push(this.ceiling_meshes[z][x].mesh);
-                }
-                if (this.floor_meshes[z] && this.floor_meshes[z][x]) {
-                    result_meshes.push(this.floor_meshes[z][x].mesh);
-                }
-            }
-        }
-        return result_meshes;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -137,14 +137,14 @@ function drawScene() {
 
     const aspect = gl.drawingBufferWidth / gl.drawingBufferHeight;
     const fovx = 2 * Math.atan(aspect * Math.tan(FOV / 2));
-    height_map.activateMeshesInView(camera.position, camera.getForward(), fovx, 10, 4);
+    height_map.activateMeshesInView(gl, camera.position, camera.getForward(), fovx, 12, 4);
 
     for (let z = 0; z < height_map.z_chunks; z++) {
         for (let x = 0; x < height_map.x_chunks; x++) {
             const c_mesh = height_map.ceiling_meshes[z][x];
             if (!c_mesh.active) continue;
-            c_mesh.mesh.render(renderer, v, p);
-            height_map.floor_meshes[z][x].mesh.render(renderer, v, p);
+            c_mesh.mesh?.render(renderer, v, p);
+            height_map.floor_meshes[z][x].mesh?.render(renderer, v, p);
         }
     }
 
